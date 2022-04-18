705bc4e1e8700a808d9aa00b06c1d7aa5a96a47f
Václav Chlumský <chlumskyvaclav@gmail.com>
2022-04-19 00:20:06 +0200

    refactor ChildrenAdapter

--- app/src/main/java/digital/cesko/movapp/adapter/ChildrenAdapter.kt
+++ app/src/main/java/digital/cesko/movapp/adapter/ChildrenAdapter.kt
@@ -4,13 +4,11 @@ import android.content.Context
 import android.content.res.ColorStateList
 import android.content.res.Configuration
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import digital.cesko.movapp.R
+import digital.cesko.movapp.databinding.ChildrenItemBinding
 import digital.cesko.movapp.ui.children.ChildrenData
 
 class ChildrenAdapter (
@@ -20,66 +18,51 @@ class ChildrenAdapter (
 
     var fromUa = true
 
-    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
-        val imageChildren: ImageView = view.findViewById(R.id.image_children_main)
-        val textFrom: TextView = view.findViewById(R.id.text_children_from)
-        val textTo: TextView = view.findViewById(R.id.text_children_to)
-
-        val imageFlagFrom: ImageView = view.findViewById(R.id.image_children_flag_from)
-        val imageFlagTo: ImageView = view.findViewById(R.id.image_children_flag_to)
+    class ItemViewHolder(binding: ChildrenItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        val binding = binding
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChildrenAdapter.ItemViewHolder {
-        val adapterLayout = LayoutInflater.from(parent.context)
-            .inflate(R.layout.children_item, parent, false)
-        return ChildrenAdapter.ItemViewHolder(adapterLayout)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        return ItemViewHolder(ChildrenItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
     }
 
     override fun getItemCount(): Int {
         return dataset.size
     }
 
-    override fun onBindViewHolder(holder: ChildrenAdapter.ItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
-        var strFrom = ""
-        var strTo = ""
-
-        var flagFrom: Int
-        var flagTo: Int
 
         /* for dark theme, use white color as tint */
         when (context.resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)) {
             Configuration.UI_MODE_NIGHT_YES -> {
-                holder.imageChildren.imageTintList =  ColorStateList.valueOf(ContextCompat.getColor(context, R.color.white))
+                holder.binding.imageChildrenMain.imageTintList = ColorStateList.valueOf(ContextCompat.getColor(context, R.color.white))
             }
             else -> {
-                holder.imageChildren.imageTintList =  ColorStateList.valueOf(ContextCompat.getColor(context, R.color.black))
+                holder.binding.imageChildrenMain.imageTintList = ColorStateList.valueOf(ContextCompat.getColor(context, R.color.black))
             }
         }
 
-        holder.imageChildren.setImageDrawable(item.image)
+        holder.binding.apply {
+            imageChildrenMain.setImageDrawable(item.image)
 
-        when(fromUa) {
-            true -> {
-                strFrom = "%s [%s]".format(item.translation_from, item.transcription_from)
-                strTo = "%s [%s]".format(item.translation_to, item.transcription_to)
+            if (fromUa) {
+                textChildrenFrom.text = formatTrans(item.translation_from, item.transcription_from)
+                textChildrenTo.text = formatTrans(item.translation_to, item.transcription_to)
 
-                flagFrom = R.drawable.ua
-                flagTo = R.drawable.cz
-            }
+                imageChildrenFlagFrom.setImageResource(R.drawable.ua)
+                imageChildrenFlagTo.setImageResource(R.drawable.cz)
+            } else {
+                textChildrenFrom.text = formatTrans(item.translation_to, item.transcription_to)
+                textChildrenTo.text = formatTrans(item.translation_from, item.transcription_from)
 
-            false -> {
-                strTo = "%s [%s]".format(item.translation_from, item.transcription_from)
-                strFrom = "%s [%s]".format(item.translation_to, item.transcription_to)
-
-                flagTo = R.drawable.ua
-                flagFrom = R.drawable.cz
+                imageChildrenFlagFrom.setImageResource(R.drawable.cz)
+                imageChildrenFlagTo.setImageResource(R.drawable.ua)
             }
         }
+    }
 
-        holder.textFrom.text = strFrom
-        holder.textTo.text = strTo
-        holder.imageFlagFrom.setImageResource(flagFrom)
-        holder.imageFlagTo.setImageResource(flagTo)
+    private fun formatTrans(translation: String, transcription: String) : String {
+        return "$translation [${transcription}]"
     }
 }
