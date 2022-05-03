4f679366584b3cf0c787e6bd861d075e7d6daf36
Lubos Sytensky <80392744+LubosSytensky-cen86208@users.noreply.github.com>
2022-05-03 16:51:57 +0200

    appMenu

--- app/src/main/java/cz/movapp/app/App.kt
+++ app/src/main/java/cz/movapp/app/App.kt
@@ -1,6 +1,7 @@
 package cz.movapp.app
 
 import android.app.Application
+import android.content.SharedPreferences
 import android.os.StrictMode
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancel
@@ -12,21 +13,34 @@ fun Application.appModule() = (this as App).appModule
 class App : Application() {
 
     lateinit var appModule: AppModule
-
+    lateinit var preferences: SharedPreferences
 
     override fun onCreate() {
         super.onCreate()
-        if (BuildConfig.DEBUG){
+        if (BuildConfig.DEBUG) {
             StrictMode.enableDefaults()
         }
         appModule = AppModule(this.applicationContext)
+//        this?.getSharedPreferences(
+//            getString("R.string.preference_file_key"), Context.MODE_PRIVATE)
+
     }
 
+    fun getPreferedLanguageApp() {
+
+    }
+
+    fun setPreferedLanguageApp(language: String) {
+        val editor: SharedPreferences.Editor = preferences.edit()
+        editor.putString("language", language)
+        editor.commit()
+    }
 
     override fun onLowMemory() {
         super.onLowMemory()
         appModule.appScope.cancel("onLowMemory() called by system")
         appModule.appScope = MainScope()
     }
+
 
 }
