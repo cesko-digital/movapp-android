9834c2a9ade2a0323f035ff6ebe82d2ed86798ea
Ondrej Malek <o.malek.cz@gmail.com>
2022-05-29 22:02:07 +0200

    Bug fix: alphabet mixed up when returned to up after longer time - This might do it. I guess it switches viewmodels. This creates 2 instances instead of one

--- app/src/main/java/cz/movapp/app/ui/alphabet/AlphabetFragment.kt
+++ app/src/main/java/cz/movapp/app/ui/alphabet/AlphabetFragment.kt
@@ -36,9 +36,10 @@ class AlphabetFragment : Fragment() {
     ): View {
         val lang = mainSharedViewModel.selectedLanguage.value!!
         val app = this.requireActivity().application as App
+        val direction = getArgDirection()
         val viewModel =
-            ViewModelProvider(this, AlphabetViewModel.Factory(app, lang, getArgDirection()))
-                .get(AlphabetViewModel::class.java)
+            ViewModelProvider(this, AlphabetViewModel.Factory(app, lang, direction))
+                .get(direction.name,AlphabetViewModel::class.java)
         _binding = FragmentAlphabetBinding.inflate(inflater, container, false)
         this.viewLifecycleOwner.lifecycle.addObserver(object : DefaultLifecycleObserver {
             override fun onDestroy(owner: LifecycleOwner) {
